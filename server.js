e3fef7bdb77541fc3367f251ec3b5a608ef89527
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-03-15 11:51:00 -0500

    added pokemon database

--- server.js
+++ server.js
@@ -1,43 +1,52 @@
-//Express App
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-console.log(process.env.API_TOKEN)
+const POKEDEX = require('./pokedexDataBase/pokedex.json')
 
 const app = express()
 
 app.use(morgan('dev'))
 
-// app.use(validateBearerToken)
-
 app.use(function validateBearerToken(req, res, next) {
-   const apiToken = process.env.API_TOKEN
-   const authToken = req.get('Authorization')
-    
-   debugger
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
-    if(!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({error: 'UnAuthorized Request'})
-    }
-
-    next()
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
 })
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
-function handleGetTypes(req, res) {
-    res.json(validTypes);
-}
+app.get('/types', function handleGetTypes(req, res) {
+  res.json(validTypes)
+})
 
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!')
-}
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
 
-app.get('/types', handleGetTypes)
-app.get('/pokemon', handleGetPokemon)
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
 
-const PORT = 3000
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
 
-app.listen(PORT,  () => {
-    console.log(`Server is listening on http://localhost:${PORT}`)
-});
+  res.json(response)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
