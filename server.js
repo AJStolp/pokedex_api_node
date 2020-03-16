e19972a8a03be8428e9f5be39d108ea1b0688de3
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-03-16 17:27:47 -0500

    deploying to server

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const helmet = require('helmet')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -21,6 +22,17 @@ app.use(function validateBearerToken(req, res, next) {
   // move to the next middleware
   next()
 })
+
+app.use((error, req, res, next) => {
+  let response
+  if(process.env.NODE_ENV === 'production') {
+    response = {error: { message: 'server error'}}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+const PORT = process.env.PORT || 8000
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
@@ -49,7 +61,6 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
   res.json(response)
 })
 
-const PORT = 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
